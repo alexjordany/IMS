b0b6e8df0ae0fab6c119b2662e9201930c021a6e
Alex Jordany Parrales López <alexjordany@outlook.com>
2022-03-19 21:29:14 -0600

    Implementing View Inventory page

--- IMS.Application/Features/Inventories/Queries/ViewInventoriesByName/ViewInventoriesByNameVM.cs
+++ IMS.Application/Features/Inventories/Queries/ViewInventoriesByName/ViewInventoriesByNameVM.cs
@@ -4,5 +4,8 @@
     {
         public int InventoryId { get; set; }
         public string InventoryName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
     }
 }
